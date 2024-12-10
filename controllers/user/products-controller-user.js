f167603110d82adcf2a4d5693f94e0cc8e19ee9a
saurav84ya <sauravkumarobra50@gmail.com>
2024-12-10 13:42:13 +0530

    rating added

--- controllers/user/products-controller-user.js
+++ controllers/user/products-controller-user.js
@@ -26,26 +26,73 @@ const getFilteredProducts = async (req, res) => {
 };
 
 
-const getProductDetails = async (req,res) => {
+
+const getProductDetails = async (req, res) => {
   try {
-    const {id} = req.params
-    const product = await Product.findById(id)
-    if(!product) return res.status(404).json({
-      success : true,
-      message :"product not found"
-    })
+    const { id } = req.params;
+
+    // Fetch product from the database
+    const prod_uct = await Product.findById(id);
+    if (!prod_uct)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+
+    // Calculate the average review rating
+    const totalRatings = prod_uct.reviews.reduce((sum, review) => sum + review.rating, 0);
+    const averageReview = prod_uct.reviews.length > 0
+      ? (totalRatings / prod_uct.reviews.length).toFixed(1) // Round to 1 decimal place
+      : 0;
+
+    // Attach the dynamically calculated averageReview
+    const product = {
+      ...prod_uct.toObject(),
+      averageReview: Number(averageReview),
+    };
+
+
+    // console.log("product",product)
 
     res.status(200).json({
-      success : true ,
-      data : product
-    })
+      success: true,
+      data: product,
+    });
   } catch (error) {
-    //(error)
+    console.error(error);
     res.status(500).json({
-      success : false ,
-      message : "some error occured"
-    })
+      success: false,
+      message: "Some error occurred",
+    });
   }
-}
+};
+
+
+
+
+// const getProductDetails = async (req,res) => {
+//   try {
+//     const {id} = req.params
+//     const product = await Product.findById(id)
+//     if(!product) return res.status(404).json({
+//       success : true,
+//       message :"product not found"
+//     })
+
+
+//     console.log('product',product)
+ 
+//     res.status(200).json({
+//       success : true ,
+//       data : product
+//     })
+//   } catch (error) {
+//     //(error)
+//     res.status(500).json({
+//       success : false ,
+//       message : "some error occured"
+//     })
+//   }
+// }
 
 module.exports = { getFilteredProducts ,getProductDetails };
